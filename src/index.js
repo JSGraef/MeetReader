607c9f3df369e2628d86bf17fc78e594645518e7
JSGraef <jg@joshgraef.com>
2017-04-10 15:23:16 -0400

    Total refresh, start from scratch.

--- src/index.js
+++ src/index.js
@@ -3,17 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
-const Providers = () => {
-  return ( 
-    <MuiThemeProvider>
-      <App />
-    </MuiThemeProvider>
-  )
-}
-
 ReactDOM.render(
-  <Providers />,
+  <App />,
   document.getElementById('root')
 );
