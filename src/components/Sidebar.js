5bedb570943483339616e4de9e6cec3af20ae87d
JSGraef <jg@joshgraef.com>
2017-04-18 10:14:00 -0400

    Make sidebar menu route-specific

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,20 +3,27 @@ import {Link} from 'react-router-dom';
 import U from './MeetReader/utils';
 import {Menu, Icon} from 'antd';
 const {SubMenu} = Menu;
+import {Route} from 'react-router-dom';
 
-// Lists the individual event
-const Sidebar = (props) => {
-
-    if(props.events.length <= 0 || props.teams.length <= 0)
+const TeamsMenu = (props) => {
+    if(props.teams === undefined)
         return null;
 
-    return (             
-        <Menu mode="inline" defaultOpenKeys={['teams']} style={{ height: '100%' }} >
-
+    return (
+        <Menu mode="inline" style={{ height: '100%' }}>
             <SubMenu key="teams" title={<span><Icon type="usergroup-add" />Teams</span>}>
                 { props.teams.map( team => { return <Menu.Item key={team.teamCode}><Link to={`/meet/team/${team.teamCode}`}>{team.teamCode}</Link></Menu.Item> } ) }
             </SubMenu>
+        </Menu>
+    );
+}
 
+const EventsMenu = (props) => {
+    if(props.events === undefined)
+        return null;
+
+    return (
+        <Menu mode="inline" style={{ height: '100%' }}>
             <SubMenu key="events" title={<span><Icon type="solution" />Events</span>}>
                 <Menu.Item key="showall"><Link to={`/meet/events`}>Show All</Link></Menu.Item>
                     { props.events.map( event => { 
@@ -25,8 +32,21 @@ const Sidebar = (props) => {
                         return <Menu.Item key={event[0].eventNum}><Link to={`/meet/events/${event[0].eventNum}`}>{U.parseEventTitle(event[0])}</Link></Menu.Item> 
                     })}
             </SubMenu>
-
         </Menu>
+    );
+}
+
+// Lists the individual event
+const Sidebar = (props) => {
+
+    if(props.events.length <= 0 || props.teams.length <= 0)
+        return null;
+
+    return (             
+        <div>
+            <Route path="/meet" render={ (p) => <TeamsMenu teams={props.teams} {...p} /> } />
+            <Route path="/meet" render={ (p) => <EventsMenu events={props.events} {...p} /> } />
+        </div>
     );
 }
 
