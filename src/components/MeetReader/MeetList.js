d271be766e1cabdb5ee83ef0a09cf929a66ac767
JSGraef <jg@joshgraef.com>
2017-04-28 15:44:36 -0400

    Add database meet list and a way to delete meets

--- src/components/MeetReader/MeetList.js
+++ src/components/MeetReader/MeetList.js
@@ -3,6 +3,8 @@ import {Link} from 'react-router-dom';
 
 import './MeetReader.css';
 
+import {Button, Table, Popconfirm, message} from 'antd';
+
 // Firebase 
 import {config} from '../../config/constants';
 import Rebase  from 're-base';
@@ -19,24 +21,74 @@ class MeetList extends Component {
   componentDidMount() {
     var path = 'MRVAC/meets/meetlist';
 
-    base.fetch(path, {
+    base.listenTo(path, {
         context : this,
         then(data) {
-            this.setState({meets: data})
+            this.setState({meets: data}); // <-- throwing a warning because it's unmounted...
     }});
+  }
+
+  onDelete(meetid) {
+    if(meetid === undefined)
+        return;
+
+    base.remove(`MRVAC/meets/${meetid}`, function(err){
+        if(err){
+            message.error('Meet unable to be deleted.')
+        } else {
+            base.remove(`MRVAC/meets/meetlist/${meetid}`);
+            message.success('Meet deleted successfully.');
+        }
+    });
+
+    
   }
   
   render() {
     const meetkeys = Object.keys(this.state.meets);
+
+    const columns = [{
+            title: 'Meet Name',
+            dataIndex: 'name',
+            key: 'name',
+            render: (text, record) => <Link to={`./meet/${record.key}`}><h4>{text}</h4></Link>,
+        }, {
+            title: 'Date',
+            dataIndex: 'date',
+            key: 'date',
+        }, {
+            title: 'Action',
+            key: 'action',
+            render: (text, record) => (
+                <Popconfirm title="Delete this meet forever?" okText="Yes" onConfirm={() => this.onDelete(record.key)}>
+                    <Button type="danger">Delete</Button>
+                </Popconfirm>
+            ),
+    }];
+
+    let data = [];
+    
+    // Populate the data for the table
+    // For each meet, get an object of the data we want that corresponds to the table columns
+    if(meetkeys.length > 0) {
+        meetkeys.map( key => {
+            const meet = this.state.meets[key];
+            if(meet === undefined) 
+                return;
+
+            const meetData = {
+                key: key,
+                date: meet.meetStart,
+                name: meet.meetName
+            }
+
+            data.push(meetData);
+            return;
+        } )
+    }
+
     return (
-      <div>
-        <h2>Meets:</h2>
-        {meetkeys.map( key => {
-          const meet = this.state.meets[key];
-          if(meet === undefined) return '';
-          return <Link key={key} to={`./meet/${key}`}><h4>{meet.meetStart} - {meet.meetName}</h4></Link>
-        } )}
-      </div>
+        <Table columns={columns} dataSource={data} /> 
     )
   }
 }
