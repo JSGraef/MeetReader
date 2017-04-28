62f78083a51ffde13832a61ac68cd36295c1caa8
JSGraef <jg@joshgraef.com>
2017-04-28 15:14:46 -0400

    Upgrade packages and ensure english locale

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AuthHome from './components/AuthHome';
+import {LocaleProvider} from 'antd';
+import enUS from 'antd/lib/locale-provider/en_US';
 import './index.css';
 import {BrowserRouter as Router} from 'react-router-dom';
 
 ReactDOM.render(
-  <Router>
-    <AuthHome />
-  </Router>,
+  <LocaleProvider locale={enUS}>
+    <Router>
+      <AuthHome />
+    </Router>
+  </LocaleProvider>,
   document.getElementById('root')
 );
